b3b89e8f31690335718867381843bf68df7bad01
Alissa <138076657+fadedsparkles@users.noreply.github.com>
2024-06-11 19:03:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const emailListItems = document.querySelectorAll(".email-list li");
+    const emailContent = document.querySelector(".email-content");
+    const replyButton = document.querySelector(".reply-btn");
+
+    // Oculta a área de conteúdo da carta e o botão de resposta inicialmente
+    emailContent.style.display = "none";
+    replyButton.style.display = "none";
+
+    // Adiciona um event listener para cada item da lista de e-mails
+    emailListItems.forEach(item => {
+        item.addEventListener("click", function() {
+            // Remove a classe "clicked" de todos os itens
+            emailListItems.forEach(item => {
+                item.classList.remove("clicked");
+            });
+
+            // Adiciona a classe "clicked" ao item selecionado
+            item.classList.add("clicked");
+
+            // Oculta a exclamação do item selecionado
+            const exclamation = item.querySelector(".exclamation");
+            if (exclamation) {
+                exclamation.style.display = "none";
+            }
+
+            // Obtém o título da carta sem o ponto de exclamação
+            const title = item.childNodes[2].textContent.trim();
+
+            // Obtém a mensagem padrão da carta
+            const defaultMessage = item.querySelector(".letter-text").value;
+
+            // Atualiza o conteúdo da carta com base no item selecionado
+            emailContent.querySelector('h2').textContent = title;
+            emailContent.querySelector('p').textContent = defaultMessage;
+
+            // Exibe o conteúdo da carta e o botão de resposta
+            emailContent.style.display = "block";
+            replyButton.style.display = "block";
+        });
+    });
+});
